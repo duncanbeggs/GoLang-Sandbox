799f3f07beb13e1913bec8d6f5cedf390ed27d87
Duncan Beggs <DuncanRBeggs@gmail.com>
2017-11-08 22:26:26 +0000

    new tree program

--- Tree/binaryTree.go
+++ Tree/binaryTree.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type treeNode struct {
@@ -24,53 +23,7 @@ func newTree() *Tree {
 }
 
 func main() {
-	fmt.Println("***-Welcome to Duncan's Binary Tree!!-***")
-	fmt.Println("     *    *    ")
-	fmt.Println("*  * *   *   * ")
-	fmt.Println("**  **  ** **  ")
-	fmt.Println("  * *    **    ")
-	fmt.Println("  **    *   *  ")
-	fmt.Println("  **   **   ** ")
-	fmt.Println("    **  *  **  ")
-	fmt.Println(" **  ** ***    ")
-	fmt.Println("   **** **     ")
-	fmt.Println("     ****      ")
-	fmt.Println("     ****      ")
-	fmt.Println("     ****      ")
-	fmt.Print("New Tree Instatiating")
-	time.Sleep(1200 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(120 * time.Millisecond)
-	fmt.Print("..")
-	time.Sleep(480 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(200 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(200 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(160 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(80 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(600 * time.Millisecond)
-	fmt.Print("..")
-	time.Sleep(160 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(50 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(90 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(800 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(1800 * time.Millisecond)
-	fmt.Print("..")
-	time.Sleep(50 * time.Millisecond)
-	fmt.Print(".")
-	time.Sleep(50 * time.Millisecond)
-	fmt.Print("...")
-	time.Sleep(50 * time.Millisecond)
-	fmt.Print(".....\n")
-	time.Sleep(50 * time.Millisecond)
+	printWelcome()
 	fmt.Println("Tree Ready! What would you like to do?")
 
 
---
+++ Tree/printWelcome.go
@@ -0,0 +1,58 @@
+//Writing a custom implementation of a binary tree
+//All in one file for now. Will break out to multiple files after figuring out how to do so.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printWelcome() {
+	fmt.Println("***-Welcome to Duncan's Binary Tree!!-***")
+	fmt.Println("     *    *    ")
+	fmt.Println("*  * *   *   * ")
+	fmt.Println("**  **  ** **  ")
+	fmt.Println("  * *    **    ")
+	fmt.Println("  **    *   *  ")
+	fmt.Println("  **   **   ** ")
+	fmt.Println("    **  *  **  ")
+	fmt.Println(" **  ** ***    ")
+	fmt.Println("   **** **     ")
+	fmt.Println("     ****      ")
+	fmt.Println("     ****      ")
+	fmt.Println("     ****      ")
+	fmt.Print("New Tree Instatiating")
+	time.Sleep(1200 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(120 * time.Millisecond)
+	fmt.Print("..")
+	time.Sleep(480 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(200 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(200 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(160 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(80 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(600 * time.Millisecond)
+	fmt.Print("..")
+	time.Sleep(160 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(50 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(90 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(800 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(1800 * time.Millisecond)
+	fmt.Print("..")
+	time.Sleep(50 * time.Millisecond)
+	fmt.Print(".")
+	time.Sleep(50 * time.Millisecond)
+	fmt.Print("...")
+	time.Sleep(50 * time.Millisecond)
+	fmt.Print(".....\n")
+	time.Sleep(50 * time.Millisecond)
+}
