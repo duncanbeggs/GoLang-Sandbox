791216513e133ada0c00c1096be6e8ef0145153b
Duncan Beggs <DuncanRBeggs@gmail.com>
2017-11-08 19:58:16 +0000

    progress on tree

---
+++ Tree/binaryTree.go
@@ -0,0 +1,16 @@
+//Writing a custom implementation of a binary tree
+//All in one file for now. Will break out to multiple files after figuring out how to do so.
+package main
+
+import "fmt"
+
+type treeNode struct {
+	var leftPtr *Node
+	var rightPtr *Node
+
+
+func main() {
+	fmt.Println("One day I will be a big beautiful tree!")
+}
+
+
