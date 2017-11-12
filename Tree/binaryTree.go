20053e4c2fb538e74d6ffd6950ef6288c8a9b245
Duncan Beggs <DuncanRBeggs@gmail.com>
2017-11-12 21:13:19 +0000

    added some stuff

--- Tree/binaryTree.go
+++ Tree/binaryTree.go
@@ -25,21 +25,25 @@ func newTree() *Tree {
 func main() {
 	printWelcome()
 	fmt.Println("Tree Ready! What would you like to do?")
-	fmt.Printf("1 - Insert int\n2 - Delete int\n3 - Check for int\n4 - Print Tree\n")
-	var x int
-	switch fmt.Scan(&x) {
-	case 1:
-		fmt.Printf("Insert int> ")
-	case 2:
-		fmt.Printf("Delete int> ")
-	case 3:
-		fmt.Printf("Search for int> ")
-	case 4:
-		fmt.Printf("Print in-order(1), pre-order(2) or post-order(3)?>")
-	default:
-		fmt.Println("You fucked up. INTEGER 1-4!")
-		
-
-
+	fmt.Printf("1 - Insert int\n2 - Delete int\n3 - Check for int\n4 - Print Tree\n5 - Quit\n> ")
+	x := 0
+	for  x != 5{
+		fmt.Scan(&x)
+		switch x {
+		case 1:
+			fmt.Printf("Insert int> ")
+		case 2:
+			fmt.Printf("Delete int> ")
+		case 3:
+			fmt.Printf("Search for int> ")
+		case 4:
+			fmt.Printf("Print in-order(1), pre-order(2) or post-order(3)?>")
+		case 5:
+			fmt.Println("ByeBye!!")
+		default:
+			fmt.Printf("You fucked up. INTEGER 1-4!> ")
+			fmt.Scan(&x)
+		}
+	}
 	fmt.Println("One day I will be a big beautiful tree!")
 }
