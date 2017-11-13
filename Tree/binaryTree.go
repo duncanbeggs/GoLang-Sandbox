ba1144e310a259d1821517e8d9f793153ecc9341
Duncan Beggs <DuncanRBeggs@gmail.com>
2017-11-13 01:41:13 +0000

    added insert functionality to tree

--- Tree/binaryTree.go
+++ Tree/binaryTree.go
@@ -14,12 +14,35 @@ type treeNode struct {
 
 type Tree struct {
 	root *treeNode
+	isEmpty bool
 	//might put some more stuff in here later
 }
 
-func newTree() *Tree {
-	var t *Tree
-	return t
+func nodeInsert(intVal int, t *Tree) bool {
+	if (*t).isEmpty == true {
+		newNode := treeNode{nil, nil, intVal}
+		t.root = &newNode
+		(*t).isEmpty = false
+	} else {
+		recNodeInsert(intVal, (*t).root)
+	}
+	return true
+}
+
+func recNodeInsert(intVal int, tn *treeNode) {
+	if tn == nil {
+		newNode := treeNode{nil,nil,intVal}
+		tn = &newNode
+		return
+	}
+
+	if tn.nodeVal < intVal {//NOTE: switching from (*tn).nodeVal syntax. These are equivalent in Go
+		recNodeInsert(intVal, tn.rightPtr)
+	} else if tn.nodeVal > intVal {
+		recNodeInsert(intVal, tn.leftPtr)
+	} else {//in case of tn.nodeVal == intVal then do nothing
+		return
+	}
 }
 
 func main() {
@@ -27,23 +50,44 @@ func main() {
 	fmt.Println("Tree Ready! What would you like to do?")
 	fmt.Printf("1 - Insert int\n2 - Delete int\n3 - Check for int\n4 - Print Tree\n5 - Quit\n> ")
 	x := 0
-	for  x != 5{
+	n := 0
+	myTree := Tree{nil, true}
+	treePtr := &myTree
+	for  x != 5 {
 		fmt.Scan(&x)
 		switch x {
 		case 1:
 			fmt.Printf("Insert int> ")
+			fmt.Scan(&n)
+			nodeInsert(n, treePtr)
 		case 2:
 			fmt.Printf("Delete int> ")
+			fmt.Scan(&n)
 		case 3:
 			fmt.Printf("Search for int> ")
+			fmt.Scan(&n)
 		case 4:
 			fmt.Printf("Print in-order(1), pre-order(2) or post-order(3)?>")
+			fmt.Scan(&n)
+			switch n {
+				case 1:
+					fmt.Printf("in-order")
+				case 2:
+					fmt.Printf("pre-order")
+				case 3:
+					fmt.Printf("post-order")
+				default:
+					fmt.Println("Option not valid")
+			}
 		case 5:
 			fmt.Println("ByeBye!!")
 		default:
-			fmt.Printf("You fucked up. INTEGER 1-4!> ")
+			fmt.Printf("You fucked up. INTEGER 1-5!> ")
 			fmt.Scan(&x)
 		}
+		if x != 5 {
+			fmt.Printf("Anything else?\n> ")
+		}
 	}
-	fmt.Println("One day I will be a big beautiful tree!")
 }
+
